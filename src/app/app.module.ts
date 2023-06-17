f6ce56bd98f9ab8165bf20df7b8bf9bb318693d8
AnjaWurli <anja.wurlitzer.coding@gmail.com>
2023-06-17 16:04:22 +0200

    add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { HeaderComponent } from './header/header.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material.module';
+
 @NgModule({
-  declarations: [AppComponent, TodoComponent],
+  declarations: [AppComponent, TodoComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
