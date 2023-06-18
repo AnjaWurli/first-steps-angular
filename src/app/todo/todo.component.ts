c632b0010d40d62683155675a9fa7debf149ea59
AnjaWurli <anja.wurlitzer.coding@gmail.com>
2023-06-18 17:48:07 +0200

    add basic todo functionality

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class TodoComponent {
   hideTxt: boolean = false;
+
+  todos: { text: string; id: number; done: boolean }[] = [
+    { text: 'Pepperoni', id: 1, done: true },
+    { text: 'Extra Cheese', id: 2, done: false },
+  ];
+
+  addTodo(e: any, wantTodo: string) {
+    e.preventDefault();
+    let newTodo = { text: wantTodo, id: this.todos.length + 1, done: false };
+    if (wantTodo.length >= 5) {
+      this.todos.push(newTodo);
+    }
+  }
+
+  deleteTodos() {
+    this.todos.forEach((doneTodo, i) => {
+      if (doneTodo.done) {
+        this.todos.splice(i, 1);
+      }
+    });
+  }
 }
